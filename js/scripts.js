928c89a94af6a03843cb97de6a20f06aaecd9c58
lonalore <lonalore@freemail.hu>
2016-06-22 17:01:52 +0200

    Responsive chat modal.

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,41 @@ var eChat = eChat || {'modal': {}, 'settings': {}, 'callbacks': {}};
 
 (function ($) {
 
+    eChat.settings = {
+        'modal': {
+            'max-width': 900,
+            'max-height': 600
+        }
+    };
+
     $(document).ready(function () {
         eChat.modal = $('#eChatModal');
         eChat.modal.modal('show');
 
-        eChat.modal.on('shown.bs.modal', function (e) {
+        eChat.modal.on('show.bs.modal', function () {
+
+        });
+
+        eChat.modal.on('shown.bs.modal', function () {
             eChat.callbacks.scrollMessagesToBottom();
+            eChat.callbacks.initAutoGrow();
         });
     });
 
     eChat.callbacks.scrollMessagesToBottom = function () {
-        var $section = $('section.message-container');
-        $section.animate({scrollTop: $section.prop("scrollHeight")}, 500);
+        var $section = $('.message-container');
+        $section.stop().animate({scrollTop: $section.prop("scrollHeight")}, 500);
+    };
+
+    eChat.callbacks.initAutoGrow = function () {
+        var $textarea = $('.message-container .message-input');
+        $textarea.css('overflow', 'hidden').autogrow({
+            vertical: true,
+            horizontal: false
+        });
+        $textarea.on('keyup', function () {
+            eChat.callbacks.scrollMessagesToBottom();
+        });
     };
 
 })(jQuery);
